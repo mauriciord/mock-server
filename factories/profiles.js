125a0c3d399bb30cc93ca27c1e3b59287fbff001
Mauricio R. Duarte <mauriciotem@gmail.com>
2018-08-14 10:53:36 -0300

    config before pushing to heroku

--- factories/profiles.js
+++ factories/profiles.js
@@ -1,13 +1,23 @@
 const { manager, factory } = require('node-factory');
 
+const treatments = ['Dr.', 'Dra.', 'Sr.', 'Sra.'];
+const councils = ['CRAS', 'CRBM', 'CREFITO', 'COREN', 'CREF', 'CRF', 'CRFA', 'CRM', 'CRN', 'CRO', 'CRP', 'CRT', 'CRNT'];
+
 manager.register('Profile', faker => {
   return {
     id: faker.random.number(),
-    name: faker.name.findName(),
-    email: faker.internet.email(),
-    crm: faker.random.number(),
-    specialty: faker.company.bsAdjective(),
-    photo: faker.image.people(),
+    user: null,
+    first_name: faker.name.firstName(),
+    last_name: faker.name.lastName(),
+    phone: faker.phone.phoneNumberFormat(),
+    introduction: faker.lorem.paragraph(),
+    website: faker.internet.url(),
+    facebook: faker.internet.url(),
+    instagram: faker.internet.url(),
+    picture: faker.image.avatar(),
+    register_number: faker.random.number(),
+    treatment: treatments[Math.floor(Math.random() * treatments.length)],
+    council_name: councils[Math.floor(Math.random() * councils.length)],
   }
 });
 
--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const factory = require('./factories/profiles');
+const educations = require('./factories/educations');
+const address = require('./factories/address');
+const services = require('./factories/services');
 
-const Profile = factory('Profile').create(50);
+const numberOfResults = 50;
+
+const Profile = factory('Profile').create(numberOfResults);
+const Educations = factory('Educations').create(numberOfResults);
+const Address = factory('Address').create(numberOfResults);
+const Services = factory('Services').create(numberOfResults);
 
 module.exports = () => {
   return ({
     profiles: Profile,
+    educations: Educations,
+    address: Address,
+    services: Services,
   });
 };
