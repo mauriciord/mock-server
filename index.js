78772074ffadbee701f8af906343484983eb9d79
Mauricio R. Duarte <mauriciotem@gmail.com>
2018-08-14 11:01:05 -0300

    config before pushing to heroku 2

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
-const factory = require('./factories/profiles');
-const educations = require('./factories/educations');
-const address = require('./factories/address');
-const services = require('./factories/services');
+const { manager, factory } = require('node-factory');
+
+const treatments = ['Dr.', 'Dra.', 'Sr.', 'Sra.'];
+const councils = ['CRAS', 'CRBM', 'CREFITO', 'COREN', 'CREF', 'CRF', 'CRFA', 'CRM', 'CRN', 'CRO', 'CRP', 'CRT', 'CRNT'];
+
+manager.register('Profile', faker => {
+  return {
+    id: faker.random.number(),
+    user: null,
+    first_name: faker.name.firstName(),
+    last_name: faker.name.lastName(),
+    phone: faker.phone.phoneNumberFormat(),
+    introduction: faker.lorem.paragraph(),
+    website: faker.internet.url(),
+    facebook: faker.internet.url(),
+    instagram: faker.internet.url(),
+    picture: faker.image.avatar(),
+    register_number: faker.random.number(),
+    treatment: treatments[Math.floor(Math.random() * treatments.length)],
+    council_name: councils[Math.floor(Math.random() * councils.length)],
+  }
+});
+
+manager.register('Services', faker => {
+  return {
+    id: faker.random.number(),
+    profile: factory('Profile').create(),
+    name: faker.name.findName(),
+  }
+});
+
+manager.register('Address', faker => {
+  return {
+    id: faker.random.number(),
+    profile: factory('Profile').create(),
+    address_name: faker.address.streetAddress(),
+  }
+});
+
+manager.register('Educations', faker => {
+  return {
+    id: faker.random.number(),
+    profile: factory('Profile').create(),
+    institution: faker.company.companyName(),
+    course: faker.name.jobArea(),
+    conclusion: 2000,
+  }
+});
+
+// const factory = require('./factories/profiles');
+// const educations = require('./factories/educations');
+// const address = require('./factories/address');
+// const services = require('./factories/services');
 
 const numberOfResults = 50;
 
